5ff4a836dc022b1b1014b2a6d12d9a47af024036
Akshay Vhatkar <akshay0706vhatkar@gmail.com>
2023-02-17 23:14:34 +0530

    Personalised frontend done

--- lib/frontend/home_content/components/body.dart
+++ lib/frontend/home_content/components/body.dart
@@ -5,6 +5,8 @@ import 'package:djhackathon/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 
+import '../../components/news_card.dart';
+
 class HomeContentBody extends StatefulWidget {
   const HomeContentBody({super.key});
 
@@ -213,112 +215,6 @@ class _HomeContentBodyState extends State<HomeContentBody> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class NewsCard extends StatelessWidget {
-  const NewsCard({
-    Key? key,
-    required this.pallete,
-    required this.index,
-    required this.length,
-  }) : super(key: key);
-
-  final Pallete pallete;
-  final int index, length;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: getHeight(120),
-          decoration: BoxDecoration(
-            color: pallete.background(),
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: pallete.primaryDark().withOpacity(0.4),
-                offset: const Offset(0, 3),
-                blurRadius: 8,
-              )
-            ],
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Container(
-                  width: getWidth(60),
-                  height: getWidth(120),
-                  decoration: BoxDecoration(
-                    color: pallete.primaryDark().withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      "assets/images/mumbai.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Space",
-                          style: TextStyle(
-                            color: pallete.primaryDark(),
-                            fontSize: getWidth(14),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: getWidth(10)),
-                        Text(
-                          "Hello how do we mak wthis what we want ans do it all on our own",
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          style: TextStyle(
-                            color: pallete.primaryDark(),
-                            fontSize: getWidth(14),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const Spacer(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "30 min ago",
-                              style: TextStyle(
-                                color: pallete.primaryDark().withOpacity(0.8),
-                                fontSize: getWidth(12),
-                              ),
-                            ),
-                            Text(
-                              "500 Views",
-                              style: TextStyle(
-                                color: pallete.primaryDark().withOpacity(0.8),
-                                fontSize: getWidth(12),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-        if (index != length - 1) SizedBox(height: getWidth(20)),
-      ],
     );
   }
 }
--- lib/frontend/personalised/components/body.dart
+++ lib/frontend/personalised/components/body.dart
@@ -2,6 +2,7 @@ import 'package:djhackathon/theme.dart';
 import 'package:flutter/material.dart';
 
 import '../../../size.dart';
+import '../../components/news_card.dart';
 
 class PersonalisedBody extends StatefulWidget {
   const PersonalisedBody({super.key});
@@ -11,16 +12,32 @@ class PersonalisedBody extends StatefulWidget {
 }
 
 class _PersonalisedBodyState extends State<PersonalisedBody> {
+  int current = 0;
+
+  List<String> tabs = [
+    "Science",
+    "Technology",
+    "Gaming",
+  ];
+
+  void changeTab(int index) {
+    setState(() {
+      current = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Pallete pallete = Pallete(context);
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Text(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: getWidth(20)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
                 "Personalised",
                 style: TextStyle(
                   color: pallete.primaryDark(),
@@ -28,10 +45,105 @@ class _PersonalisedBodyState extends State<PersonalisedBody> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: getWidth(20)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      4,
+                      (index) => TabItem(
+                        pallete: pallete,
+                        index: index,
+                        current: current,
+                        onClicked: changeTab,
+                        title: index == 0 ? "For you" : tabs[index - 1],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      ...List.generate(
+                        10,
+                        (index) => NewsCard(
+                          pallete: pallete,
+                          index: index,
+                          length: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
+    );
+  }
+}
+
+class TabItem extends StatelessWidget {
+  const TabItem({
+    Key? key,
+    required this.pallete,
+    required this.index,
+    required this.current,
+    required this.onClicked,
+    required this.title,
+  }) : super(key: key);
+
+  final Pallete pallete;
+  final int index, current;
+  final Function onClicked;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: () => onClicked(index),
+          child: Container(
+            decoration: BoxDecoration(
+                color: index == current
+                    ? pallete.primaryDark()
+                    : pallete.background(),
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: index == current
+                      ? Colors.transparent
+                      : pallete.primaryDark(),
+                )),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                title,
+                style: TextStyle(
+                  color: index == current
+                      ? pallete.background()
+                      : pallete.primaryDark(),
+                  fontSize: getWidth(18),
+                ),
+              ),
+            ),
+          ),
+        ),
+        if (index != 3) SizedBox(width: getWidth(10)),
+      ],
     );
   }
 }
